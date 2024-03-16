555b62772f9a670daf702394d482fcd9f5dd6a23
Gamedolf <gamedolf@live.co.uk>
2024-03-16 02:02:00 +0000

    Update config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/rs-analysis-vite",
+  base: "/ability-damage-analysis",
   plugins: [react()],
 })
