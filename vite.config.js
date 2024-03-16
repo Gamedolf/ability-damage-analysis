6a1384b7be32eb460762dd91c3518c04c62b909b
Gamedolf <gamedolf@live.co.uk>
2024-03-16 05:28:45 +0000

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.ASSET_URL,
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.ASSET_URL,
+    plugins: [react()],
+  }
 })
