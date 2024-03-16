97f82e083193c926471aeaba7fcf517bd81ef426
Gamedolf <gamedolf@live.co.uk>
2024-03-16 05:16:27 +0000

    Fix asset url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ability-damage-analysis",
+  base: process.env.ASSET_URL,
   plugins: [react()],
 })
